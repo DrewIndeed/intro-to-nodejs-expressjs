ea5d9c74a4b33831f547891c72b811dd9e74828d
Andrew Le <s3820098@rmit.edu.vn>
2021-09-29 10:40:18 +0700

    First commit: add globals

---
+++ 1-nodejs/1-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOWS!
+
+// __dirname - path to the directory
+// __filename - file name
+// require - function to use modules(CommonJS)
+// module - info about current module(file)
+// process - info about env where the program is being executed
+
+// More at: https://www.geeksforgeeks.org/node-js-global-objects/
+
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
