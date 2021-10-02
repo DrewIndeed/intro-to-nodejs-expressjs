807b2b9669b7e7e4d47df88f8c65d6aa9126d319
Andrew Le <s3820098@rmit.edu.vn>
2021-10-02 09:28:01 +0700

    Get html from a file (lost style and functionality version)

--- 2-expressjs/app.js
+++ 2-expressjs/app.js
@@ -1,10 +1,13 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const home = readFileSync('./navbar-app/index.html');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
+    res.write(home);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
