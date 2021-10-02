43f49df610473556bef1881931f0976e4cd1d078
Andrew Le <s3820098@rmit.edu.vn>
2021-10-02 09:02:59 +0700

    Http basics

--- 2-expressjs/app.js
+++ 2-expressjs/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("User sent");
+    res.end("Homeeeeee");
+});
+
+server.listen(5000);
