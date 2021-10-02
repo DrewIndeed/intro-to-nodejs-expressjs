1f5f9053c7c2a434218d4611c25ccaccd09424db
Andrew Le <s3820098@rmit.edu.vn>
2021-10-02 09:33:13 +0700

    Get other relating contents for html file (pain version)

--- 2-expressjs/app.js
+++ 2-expressjs/app.js
@@ -1,13 +1,28 @@
 const http = require("http");
 const { readFileSync } = require("fs");
 
-const home = readFileSync('./navbar-app/index.html');
+const home = readFileSync("./navbar-app/index.html");
+const homeStyle = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(home);
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyle);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
